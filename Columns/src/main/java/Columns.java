1817a47a6ce190c2e965e38d28cc7fc358dde47c
Kiara <kiaraharris168@gmail.com>
2022-03-04 15:56:45 -0500

    Incomplete columns
    no way to have empty spaces in column

--- Columns/src/main/java/Columns.java
+++ Columns/src/main/java/Columns.java
@@ -3,8 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 
 public class Columns {
 
@@ -13,6 +16,7 @@ public class Columns {
     static boolean fileExists = false;
     static ArrayList<String> inputFile;
     static String errorMessage;
+    static int counter = 0;
 
     public static void main(String[] args) throws IOException {
 
@@ -21,9 +25,41 @@ public class Columns {
         inputFile = (ArrayList<String>) Files
                 .readAllLines(Path.of(filePath));
 
-        System.out.println(inputFile);
+        int row = 0;
+        if(inputFile.size()< 6){
+            row = 3;
+        }
+        else if( inputFile.size() < 11){
+            row = 4;
+        }
+        else{
+            row = 9;
+        }
 
+
+        ArrayList<List<String>> transformedList = new ArrayList<>();
+
+        for (int j = 0; j < inputFile.size(); j += row) {
+            if (j < inputFile.size()) {
+                transformedList.add(inputFile.subList(j, j + row));
+            } else if (j > inputFile.size()) {
+                transformedList.add(inputFile.subList(j, inputFile.size()));
+            }
+        }
+        System.out.println(transformedList);
+
+        do {
+            transformedList.stream()
+                    .forEach(l -> {
+                        if (l.size() >= counter) {
+                            System.out.printf("%-30.30s",l.get(counter));
+                        }
+                    });
+            System.out.println();
+            counter++;
+        } while(counter < transformedList.get(0).size());
     }
+
 
     static void getFile() throws IOException {
         input = new BufferedReader(new InputStreamReader(System.in));
